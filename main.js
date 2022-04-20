c020fa4b3e11e21730aa578b31c4c67b5e130781
TechnoShinobi <99727350+TechnoShinobi@users.noreply.github.com>
2022-04-20 17:41:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
+size = 0;
+RightWristx = 0;
+LeftWristx = 0;
+
 function setup() {
 video = createCapture(VIDEO);
-video.size(300, 550);
-video.position(0, 0)
+video.size(450,550);
+video.position(0,0)
 
-    canvas = createCanvas(550, 500);
-    canvas.position(560, 120);
+canvas = createCanvas(550,450);
+canvas.position(560,120);
 
-    posenet = ml5.poseNet(video, modelLoaded);
-    posenet.on('pose', gotPoses);
+posenet = ml5.poseNet(video,modelLoaded);
+posenet.on('pose',gotPoses);
 }
 
 
@@ -19,13 +23,40 @@ console.log("POSENET IS WORKING SUCCESFULLY");
 
 
 function gotPoses(results) {
-    if (results.length > 0) {
-        console.log(results);
-    }
+if (results.length > 0) {
+console.log(results);
+nosex = results[0].pose.nose.x;
+nosey = results[0].pose.nose.y;
+RightWristx = results[0].pose.rightWrist.x; 
+LeftWristx = results[0].pose.leftWrist.x;
+
+size = floor(LeftWristx - RightWristx)
+
+} 
 }
 
 function draw() {
-    background("black")
+background("white")
+textSize(size)
+text("TECHNOSHINOBI",15,80)
+
 
 
 }
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
